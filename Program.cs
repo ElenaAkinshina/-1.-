a606dda8a341753c497756d95a2c05f4b5032e5d
елена <el.bukreeva48 @yandex.ru>
2022-12-16 17:16:59 +0300

    сделаны всетри задачи на семинаре

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@
 //a = 9; b = -3 -> да
 //a = -3 b = 9 -> нет
 
-Console.Write("Введи первое число: ");
+/*Console.Write("Введи первое число: ");
 int num_1 = int.Parse(Console.ReadLine()!);
 Console.Clear();
 Console.WriteLine("Введи второе число: ");
@@ -44,4 +44,51 @@ if(num_1 == Convert.ToInt32(Math.Pow(num_2, 2))){
 }
 else{
     Console.WriteLine($"Число {num_1} не является квадратом {num_2}");
+}*/
+
+//2. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+//3 -> Среда
+//5 ->  Пятница
+
+/*Console.Write("Введите номер дня от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!);
+if(num == 1){
+    Console.WriteLine("Понедельник");
 }
+else if(num == 2){
+    Console.WriteLine("Вторник");
+}
+else if(num == 3){
+    Console.WriteLine("Среда");
+}
+else if(num == 4){
+    Console.WriteLine("Четверг");
+}
+else if(num == 5){
+    Console.WriteLine("Пятница");
+}
+else if(num == 6){
+    Console.WriteLine("Суббота");
+}
+else if(num == 7){
+    Console.WriteLine("Воскресенье");
+}
+else{
+    Console.WriteLine("Введите число от 1 до 7!!!");
+}*/
+
+
+//3. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+    
+//   456 -> 6
+    
+// 782 -> 2
+    
+//  918 -> 8
+
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = num % 10;
+Console.Write($"Последняя цифра числа {num}: {result}");
